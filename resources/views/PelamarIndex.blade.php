3613998896d58b974c8b51eb6ec6af200a73b215
Vaqieh <vaqiehmuwafvaq@gmail.com>
2024-11-11 20:32:01 +0700

    qieh

--- resources/views/PelamarIndex.blade.php
+++ resources/views/PelamarIndex.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pelamar Page</title>
 </head>
 <body>
-    ANJAAAAAAAAAAAAAAAAAAY
+    <h1>Selamat datang di Halaman Pelamar!</h1>
+    <p>Halaman ini ditampilkan tanpa menggunakan controller.</p>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PelamarController;
 
-Route::get('/pelamar', function () {
-    return view('PelamarIndex');
-});
+Route::get('/pelamar', [PelamarController::class, 'index']);
+
