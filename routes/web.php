6ef2469364b28cce7f9fda4dd458e9efc23e60c0
naylaarr <naylarinaldi413@gmail.com>
2024-11-21 16:55:25 +0700

    nayla

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PelamarController;
+use App\Http\Controllers\PengunjungController;
 
 Route::get('/pelamar', [PelamarController::class, 'index']);
+Route::get('/pengunjung', [PengunjungController::class, 'index']);
 
