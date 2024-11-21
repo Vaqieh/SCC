b1a7860625029fb9548be24e4da20c819bf9ac57
FerdinandKurniaLaia <ferdinandkurnia08@gmail.com>
2024-11-21 18:30:47 +0700

    ferdinand

---
+++ app/Http/Controllers/LamarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lamar;
+use App\Http\Requests\StoreLamarRequest;
+use App\Http\Requests\UpdateLamarRequest;
+
+class LamarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLamarRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lamar $lamar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lamar $lamar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLamarRequest $request, Lamar $lamar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lamar $lamar)
+    {
+        //
+    }
+}
