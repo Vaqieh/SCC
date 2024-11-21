500aff7593eb33b2f6019bd9f92d187c032880bb
FerdinandKurniaLaia <ferdinandkurnia08@gmail.com>
2024-11-21 17:50:31 +0700

    Ferdinand

---
+++ database/migrations/2024_11_21_101910_create_admins_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('admin_nama');
+            $table->string('admin_nohp');
+            $table->string('email');
+            $table->timestamps('login_history');
+            $table->timestamps('logout_history');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
