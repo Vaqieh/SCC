3e7a65d56c273ba30a98b08d918cc2878b125ad1
Vaqieh <vaqiehmuwafvaq@gmail.com>
2024-11-25 10:54:39 +0700

    vaqieh

---
+++ app/Http/Controllers/KelolaPerusahaanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KelolaPerusahaan;
+use App\Http\Requests\StoreKelolaPerusahaanRequest;
+use App\Http\Requests\UpdateKelolaPerusahaanRequest;
+
+class KelolaPerusahaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKelolaPerusahaanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KelolaPerusahaan $kelolaPerusahaan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KelolaPerusahaan $kelolaPerusahaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKelolaPerusahaanRequest $request, KelolaPerusahaan $kelolaPerusahaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KelolaPerusahaan $kelolaPerusahaan)
+    {
+        //
+    }
+}
