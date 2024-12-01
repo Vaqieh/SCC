b469f5b900a76570c49171081e7f489846820cb4
TatiavierzaraRizky <tatiavierzara23ti@mahasiswa.pcr.ac.id>
2024-12-01 17:57:08 +0700

    updatetatiaa

--- resources/views/PelamarIndex.blade.php
+++ resources/views/PelamarIndex.blade.php
@@ -27,7 +27,7 @@
 
                         <div class="hero-buttons">
                             <a href="#about" class="btn btn-primary me-0 me-sm-2 mx-1">Get Started</a>
-                            <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8"
+                            <a href="https://youtu.be/wGr2BeA5VoI?feature=shared"
                                 class="btn btn-link mt-2 mt-sm-0 glightbox">
                                 <i class="bi bi-play-circle me-1"></i>
                                 Play Video
@@ -38,7 +38,7 @@
 
                 <div class="col-lg-6">
                     <div class="hero-image" data-aos="zoom-out" data-aos-delay="300">
-                        <img src="assets/img/illustration-1.webp" alt="Hero Image" class="img-fluid">
+                        <img src="assets/img/gedung_pcr" alt="Politeknik Caltex Riau" class="img-fluid">
 
                         <div class="customers-badge">
                             <div class="customer-avatars">
@@ -52,6 +52,53 @@
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="row stats-row gy-4 mt-5" data-aos="fade-up" data-aos-delay="500">
+              <div class="col-lg-3 col-md-6">
+                <div class="stat-item">
+                  <div class="stat-icon">
+                    <i class="bi bi-trophy"></i>
+                  </div>
+                  <div class="stat-content">
+                    <h4>3x Won Awards</h4>
+                    <p class="mb-0">Vestibulum ante ipsum</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="stat-item">
+                  <div class="stat-icon">
+                    <i class="bi bi-briefcase"></i>
+                  </div>
+                  <div class="stat-content">
+                    <h4>6.5k Faucibus</h4>
+                    <p class="mb-0">Nullam quis ante</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="stat-item">
+                  <div class="stat-icon">
+                    <i class="bi bi-graph-up"></i>
+                  </div>
+                  <div class="stat-content">
+                    <h4>80k Mauris</h4>
+                    <p class="mb-0">Etiam sit amet orci</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 col-md-6">
+                <div class="stat-item">
+                  <div class="stat-icon">
+                    <i class="bi bi-award"></i>
+                  </div>
+                  <div class="stat-content">
+                    <h4>6x Phasellus</h4>
+                    <p class="mb-0">Vestibulum ante ipsum</p>
+                  </div>
+                </div>
+              </div>
             </div>
 
             <div class="row stats-row gy-4 mt-5" data-aos="fade-up" data-aos-delay="500">
