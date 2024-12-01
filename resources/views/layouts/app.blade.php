8c022d8a8374667dc7d5ddbab84aa2d91e4c01a6
TatiavierzaraRizky <tatiavierzara23ti@mahasiswa.pcr.ac.id>
2024-12-01 17:09:56 +0700

    updatetatiw

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,40 @@
 
 <body>
     <div id="app">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-md shadow-sm mb-0" style="background-color: #9CD5FF;">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto"></ul>
+
+                    <header id="header" class="header d-flex align-items-center fixed-top">
+                        <div
+                            class="header-container container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+                            <a href="#" class="logo d-flex align-items-center me-auto me-xl-0">
+                                <!-- Uncomment the line below if you also wish to use an image logo -->
+                                <!-- <img src="ilanding/img/logo.png" alt=""> -->
+                                <img src="{{ url('image/logo_scc (1).png') }}" alt="Logo SCC">
+
+                            </a>
+
+                            <nav id="navmenu" class="navmenu">
+                                <ul>
+                                    <li><a href="#hero" class="active">Dashboard</a></li>
+                                    <li><a href="#features">Lowongan</a></li>
+                                    <li><a href="#about">Tentang Kami</a></li>
+                                </ul>
+                                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                            </nav>
+                        </div>
+                    </header>
                     <!-- Content -->
                     <main>
                         @yield('content')
@@ -47,12 +81,12 @@
                     <footer id="footer" class="footer">
 
                         <div class="container footer-top">
-                            <div class="row gy-4">
+                            <div class="row gy-1">
                                 <div class="col-lg-4 col-md-6 footer-about">
                                     <a href="index.html" class="logo d-flex align-items-center">
                                         <span class="sitename">SCC</span>
                                     </a>
-                                    <div class="footer-contact pt-1">
+                                    <div class="footer-contact pt-6">
                                         <p class="mt-1"><strong>Sumatera Career Center</strong> adalah Career Center
                                             yang berpusat di
                                             Politeknik Caltex Riau. Tujuan kami adalah menjadi pusat informasi karir
@@ -91,37 +125,24 @@
                                 </div>
                             </div>
                         </div>
+                </div>
 
-                        <div class="container copyright text-center mt-4">
-                            <p>© <span>Copyright</span> <strong class="px-1 sitename">iLanding</strong> <span>All Rights
-                                    Reserved</span>
-                            </p>
-                            <div class="credits">
-                                <!-- All the links in the footer should remain intact. -->
-                                <!-- You can delete the links only if you've purchased the pro version. -->
-                                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                            </div>
-                        </div>
+                </footer>
 
-                    </footer>
+                <!-- Scroll Top -->
+                <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
+                        class="bi bi-arrow-up-short"></i></a>
 
-                    <!-- Scroll Top -->
-                    <a href="#" id="scroll-top"
-                        class="scroll-top d-flex align-items-center justify-content-center"><i
-                            class="bi bi-arrow-up-short"></i></a>
+                <!-- Vendor JS Files -->
+                <script src="{{ asset('iLanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+                <script src="{{ asset('iLanding/assets/vendor/php-email-form/validate.js') }}"></script>
+                <script src="{{ asset('iLanding/assets/vendor/aos/aos.js') }}"></script>
+                <script src="{{ asset('iLanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+                <script src="{{ asset('iLanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+                <script src="{{ asset('iLanding/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
 
-                    <!-- Vendor JS Files -->
-                    <script src="{{ asset('iLanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-                    <script src="{{ asset('iLanding/assets/vendor/php-email-form/validate.js') }}"></script>
-                    <script src="{{ asset('iLanding/assets/vendor/aos/aos.js') }}"></script>
-                    <script src="{{ asset('iLanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-                    <script src="{{ asset('iLanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-                    <script src="{{ asset('iLanding/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-
-                    <!-- Main JS File -->
-                    <script src="{{ asset('iLanding/assets/js/main.js') }}"></script>
+                <!-- Main JS File -->
+                <script src="{{ asset('iLanding/assets/js/main.js') }}"></script>
 
 </body>
 
