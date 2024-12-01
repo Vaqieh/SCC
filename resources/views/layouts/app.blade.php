59f235d14c90476bbace9a12f4caece16acc5bf3
TatiavierzaraRizky <tatiavierzara23ti@mahasiswa.pcr.ac.id>
2024-12-01 21:05:27 +0700

    updatetataa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
-=======
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -57,16 +57,19 @@
     <!-- Custom CSS -->
     <style>
         /* Atur margin dan padding agar tidak ada jarak */
-        html, body {
+        html,
+        body {
             margin: 0;
             padding: 0;
             width: 100%;
             height: 100%;
-            background-color: #add8e6; /* Background untuk keseluruhan halaman */
+            background-color: #add8e6;
+            /* Background untuk keseluruhan halaman */
         }
 
         nav {
-            margin-bottom: 0 !important; /* Hilangkan margin bawah dari nav */
+            margin-bottom: 0 !important;
+            /* Hilangkan margin bawah dari nav */
         }
 
         main {
@@ -81,7 +84,6 @@
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         }
     </style>
->>>>>>> 10f57d9e29c3d553666e0c6416778cfb1ef9c4a7
 </head>
 
 <body>
@@ -99,10 +101,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto"></ul>
 
-<<<<<<< HEAD
-    <header id="header" class="header d-flex align-items-center fixed-top">
-        <div
-            class="header-container container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+                    <header id="header" class="header d-flex align-items-center fixed-top">
+                        <div
+                            class="header-container container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
 
                             <a href="#" class="logo d-flex align-items-center me-auto me-xl-0">
                                 <!-- Uncomment the line below if you also wish to use an image logo -->
@@ -120,124 +121,128 @@
                                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
                             </nav>
                         </div>
-                    </header>
-                    <!-- Content -->
-                    <main>
-                        @yield('content')
-                    </main>
+                        </header>
+                        <!-- Content -->
+                        <main>
+                            @yield('content')
+                        </main>
 
-                    <footer id="footer" class="footer">
+                        <footer id="footer" class="footer">
 
-                        <div class="container footer-top">
-                            <div class="row gy-1">
-                                <div class="col-lg-4 col-md-6 footer-about">
-                                    <a href="index.html" class="logo d-flex align-items-center">
-                                        <span class="sitename">SCC</span>
-                                    </a>
-                                    <div class="footer-contact pt-6">
-                                        <p class="mt-1"><strong>Sumatera Career Center</strong> adalah Career Center
-                                            yang berpusat di
-                                            Politeknik Caltex Riau. Tujuan kami adalah menjadi pusat informasi karir
-                                            yang lengkap dan terpercaya bagi para
-                                            pencari kerja maupun perusahaan.
-                                            <span></span>
-                                        </p>
-                                        <p><strong>Email:</strong> <span>pcr@gmail.com</span></p>
+                            <div class="container footer-top">
+                                <div class="row gy-1">
+                                    <div class="col-lg-4 col-md-6 footer-about">
+                                        <a href="index.html" class="logo d-flex align-items-center">
+                                            <span class="sitename">SCC</span>
+                                        </a>
+                                        <div class="footer-contact pt-6">
+                                            <p class="mt-1"><strong>Sumatera Career Center</strong> adalah Career
+                                                Center
+                                                yang berpusat di
+                                                Politeknik Caltex Riau. Tujuan kami adalah menjadi pusat informasi karir
+                                                yang lengkap dan terpercaya bagi para
+                                                pencari kerja maupun perusahaan.
+                                                <span></span>
+                                            </p>
+                                            <p><strong>Email:</strong> <span>pcr@gmail.com</span></p>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-4 col-md-6 footer-links">
+                                        <h4>Alamat Kantor</h4>
+                                        <ul>
+                                            <li><a href="#">Jl. Umban Sari (Patin) No. 1
+
+                                                    Rumbai, Pekanbaru, Riau
+
+                                                    Indonesia
+
+                                                    28265</a></li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="col-lg-4 col-md-6 footer-links">
+                                        <h4>Kontak</h4>
+                                        <ul>
+                                            <li><a href="#">Telp : 08117574101 & (0761) - 554224/a></li>
+                                            <li> Email : scc@pcr.ac.id </li>
+                                        </ul>
+                                        <div class="social-links d-flex mt-4">
+                                            <a href=""><i class="bi bi-twitter-x"></i></a>
+                                            <a href=""><i class="bi bi-facebook"></i></a>
+                                            <a href=""><i class="bi bi-instagram"></i></a>
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="col-lg-4 col-md-6 footer-links">
-                                    <h4>Alamat Kantor</h4>
-                                    <ul>
-                                        <li><a href="#">Jl. Umban Sari (Patin) No. 1
-
-                                                Rumbai, Pekanbaru, Riau
-
-                                                Indonesia
-
-                                                28265</a></li>
-                                    </ul>
+                            <div class="container copyright text-center mt-4">
+                                <p>© <span>Copyright</span> <strong class="px-1 sitename">iLanding</strong> <span>All
+                                        Rights Reserved</span>
+                                </p>
+                                <div class="credits">
+                                    <!-- All the links in the footer should remain intact. -->
+                                    <!-- You can delete the links only if you've purchased the pro version. -->
+                                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                                    <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+                                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
                                 </div>
+                            </div>
 
-                <div class="col-lg-4 col-md-6 footer-links">
-                    <h4>Kontak</h4>
-                    <ul>
-                        <li><a href="#">Telp : 08117574101 & (0761) - 554224/a></li>
-                          <li>  Email : scc@pcr.ac.id </li>
-                        </ul>
-                    <div class="social-links d-flex mt-4">
-                        <a href=""><i class="bi bi-twitter-x"></i></a>
-                        <a href=""><i class="bi bi-facebook"></i></a>
-                        <a href=""><i class="bi bi-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </footer>
 
-        <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1 sitename">iLanding</strong> <span>All Rights Reserved</span>
-            </p>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you've purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
-        </div>
+                        <!-- Scroll Top -->
+                        <a href="#" id="scroll-top"
+                            class="scroll-top d-flex align-items-center justify-content-center"><i
+                                class="bi bi-arrow-up-short"></i></a>
 
-    </footer>
+                        <!-- Vendor JS Files -->
+                        <script src="{{ asset('iLanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+                        <script src="{{ asset('iLanding/assets/vendor/php-email-form/validate.js') }}"></script>
+                        <script src="{{ asset('iLanding/assets/vendor/aos/aos.js') }}"></script>
+                        <script src="{{ asset('iLanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+                        <script src="{{ asset('iLanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+                        <script src="{{ asset('iLanding/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
 
-                <!-- Scroll Top -->
-                <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i
-                        class="bi bi-arrow-up-short"></i></a>
+                        <!-- Main JS File -->
+                        <script src="{{ asset('iLanding/assets/js/main.js') }}"></script>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ms-auto">
+                            @guest
+                                @if (Route::has('login'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    </li>
+                                @endif
 
-                <!-- Vendor JS Files -->
-                <script src="{{ asset('iLanding/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-                <script src="{{ asset('iLanding/assets/vendor/php-email-form/validate.js') }}"></script>
-                <script src="{{ asset('iLanding/assets/vendor/aos/aos.js') }}"></script>
-                <script src="{{ asset('iLanding/assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
-                <script src="{{ asset('iLanding/assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
-                <script src="{{ asset('iLanding/assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-
-    <!-- Main JS File -->
-    <script src="{{ asset('iLanding/assets/js/main.js') }}"></script>
-
-=======
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                        role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
                 </div>
             </div>
         </nav>
@@ -245,9 +250,8 @@
         <main class="py-4" style="margin: 0; padding: 0;">
             @yield('content')
         </main>
-        
+
     </div>
->>>>>>> 10f57d9e29c3d553666e0c6416778cfb1ef9c4a7
 </body>
 
 </html>
