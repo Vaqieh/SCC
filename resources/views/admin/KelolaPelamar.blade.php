3b0c42877ef00919a21b3a5c926c3ea1f5220c43
FerdinandKurniaLaia <ferdinandkurnia08@gmail.com>
2024-12-02 11:56:44 +0700

    ferr

--- resources/views/admin/KelolaPelamar.blade.php
+++ resources/views/admin/KelolaPelamar.blade.php
@@ -1,222 +1,101 @@
 @extends('layouts.admin', ['title' => 'Kelola Data Pelamar'])
 @section('content')
-<div class="layout-wrapper">
-
-    <!-- header -->
-    <div class="header">
-    <div class="menu-toggle-btn"> <!-- Menu close button for mobile devices -->
-        <a href="#">
-            <i class="bi bi-list"></i>
-        </a>
-    </div>
-    <!-- Logo -->
-    <a href="./dashboard.html" class="logo">
-        <img width="100" src="../logo.png" alt="logo">
-    </a>
-    <!-- ./ Logo -->
-    <div class="page-title">Orders</div>
-    <form class="search-form">
-        <div class="input-group">
-            <button class="btn btn-outline-light" type="button" id="button-addon1">
-                <i class="bi bi-search"></i>
-            </button>
-            <input type="text" class="form-control" placeholder="Search..."
-                   aria-label="Example text with button addon" aria-describedby="button-addon1">
-            <a href="#" class="btn btn-outline-light close-header-search-bar">
-                <i class="bi bi-x"></i>
-            </a>
-        </div>
-    </form>
-    <div class="header-bar ms-auto">
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item">
-                <a href="#" class="nav-link nav-link-notify" data-count="2" data-sidebar-target="#notifications">
-                    <i class="bi bi-bell icon-lg"></i>
-                </a>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link nav-link-notify" data-count="3" data-bs-toggle="dropdown">
-                    <i class="bi bi-cart2 icon-lg"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0">
-                    <h6 class="m-0 px-4 py-3 border-bottom">Shopping Cart</h6>
-                    <div class="dropdown-menu-body">
-                        <div class="list-group list-group-flush">
-                            <div class="list-group-item d-flex align-items-center">
-                                <a href="#" class="text-danger me-3" title="Remove">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                                <a href="#" class="me-3 flex-shrink-0 ">
-                                    <img src="../images/products/3.jpg" class="rounded" width="60"
-                                         alt="...">
-                                </a>
-                                <div>
-                                    <h6>Digital clock</h6>
-                                    <div>1 x $1.190,90</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="list-group list-group-flush">
-                            <div class="list-group-item d-flex align-items-center">
-                                <a href="#" class="text-danger me-3" title="Remove">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                                <a href="#" class="me-3 flex-shrink-0 ">
-                                    <img src="../images/products/4.jpg" class="rounded" width="60"
-                                         alt="...">
-                                </a>
-                                <div>
-                                    <h6>Toy Car</h6>
-                                    <div>1 x $139.58</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="list-group list-group-flush">
-                            <div class="list-group-item d-flex align-items-center">
-                                <a href="#" class="text-danger me-3" title="Remove">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                                <a href="#" class="me-3 flex-shrink-0 ">
-                                    <img src="../images/products/5.jpg" class="rounded" width="60"
-                                         alt="...">
-                                </a>
-                                <div>
-                                    <h6>Sunglasses</h6>
-                                    <div>2 x $50,90</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="list-group list-group-flush">
-                            <div class="list-group-item d-flex align-items-center">
-                                <a href="#" class="text-danger me-3" title="Remove">
-                                    <i class="bi bi-trash"></i>
-                                </a>
-                                <a href="#" class="me-3 flex-shrink-0 ">
-                                    <img src="../images/products/6.jpg" class="rounded" width="60"
-                                         alt="...">
-                                </a>
-                                <div>
-                                    <h6>Cake</h6>
-                                    <div>1 x $10,50</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <h6 class="m-0 px-4 py-3 border-top small">Sub Total : <strong
-                            class="text-primary">$1.442,78</strong></h6>
-                </div>
-            </li>
-            <li class="nav-item ms-3">
-                            </li>
-        </ul>
-    </div>
-    <!-- Header mobile buttons -->
-    <div class="header-mobile-buttons">
-        <a href="#" class="search-bar-btn">
-            <i class="bi bi-search"></i>
-        </a>
-        <a href="#" class="actions-btn">
-            <i class="bi bi-three-dots"></i>
-        </a>
-    </div>
-    <!-- ./ Header mobile buttons -->
-</div>
-<div class="content ">
     
-    <div class="mb-4">
-        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="#">
-                        <i class="bi bi-globe2 small me-2"></i> Dashboard
-                    </a>
-                </li>
-                <li class="breadcrumb-item active" aria-current="page">Orders</li>
-            </ol>
-        </nav>
-    </div>
 
-    <div class="card">
-        <div class="card-body">
-            <div class="d-md-flex gap-4 align-items-center">
-                <div class="d-none d-md-flex">All Orders</div>
-                <div class="d-md-flex gap-4 align-items-center">
-                    <form class="mb-3 mb-md-0">
-                        <div class="row g-3">
-                            <div class="col-md-3">
-                                <select class="form-select">
-                                    <option>Sort by</option>
-                                    <option value="desc">Desc</option>
-                                    <option value="asc">Asc</option>
-                                </select>
-                            </div>
-                            <div class="col-md-3">
-                                <select class="form-select">
-                                    <option value="10">10</option>
-                                    <option value="20">20</option>
-                                    <option value="30">30</option>
-                                    <option value="40">40</option>
-                                    <option value="50">50</option>
-                                </select>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search">
-                                    <button class="btn btn-outline-light" type="button">
-                                        <i class="bi bi-search"></i>
-                                    </button>
+            <div class="mb-4">
+                <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="#">
+                                <i class="bi bi-globe2 small me-2"></i> Dashboard
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">Orders</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-md-flex gap-4 align-items-center">
+                        <div class="d-none d-md-flex">All Orders</div>
+                        <div class="d-md-flex gap-4 align-items-center">
+                            <form class="mb-3 mb-md-0">
+                                <div class="row g-3">
+                                    <div class="col-md-3">
+                                        <select class="form-select">
+                                            <option>Sort by</option>
+                                            <option value="desc">Desc</option>
+                                            <option value="asc">Asc</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <select class="form-select">
+                                            <option value="10">10</option>
+                                            <option value="20">20</option>
+                                            <option value="30">30</option>
+                                            <option value="40">40</option>
+                                            <option value="50">50</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="Search">
+                                            <button class="btn btn-outline-light" type="button">
+                                                <i class="bi bi-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
+                            </form>
+                        </div>
+                        <div class="dropdown ms-auto">
+                            <a href="#" data-bs-toggle="dropdown" class="btn btn-primary dropdown-toggle"
+                                aria-haspopup="true" aria-expanded="false">Actions</a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a href="#" class="dropdown-item">Action</a>
+                                <a href="#" class="dropdown-item">Another action</a>
+                                <a href="#" class="dropdown-item">Something else here</a>
                             </div>
                         </div>
-                    </form>
-                </div>
-                <div class="dropdown ms-auto">
-                    <a href="#" data-bs-toggle="dropdown"
-                       class="btn btn-primary dropdown-toggle"
-                       aria-haspopup="true" aria-expanded="false">Actions</a>
-                    <div class="dropdown-menu dropdown-menu-end">
-                        <a href="#" class="dropdown-item">Action</a>
-                        <a href="#" class="dropdown-item">Another action</a>
-                        <a href="#" class="dropdown-item">Something else here</a>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="table-responsive">
-        <table class="table table-custom table-lg mb-0" id="orders">
-            <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Jenis Kelamin</th>
-                <th>Alamat</th>
-                <th>Email</th>
-                <th>Contact</th></th>
-                <th class="text-end">Actions</th>
-            </tr>
-            </thead>
-        </table>
-    </div>
+            <div class="table-responsive">
+                <table class="table table-custom table-lg mb-0" id="orders">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Alamat</th>
+                            <th>Email</th>
+                            <th>Contact</th>
+                            </th>
+                            <th class="text-end">Actions</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
 
-    <nav class="mt-4" aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+            <nav class="mt-4" aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
-    </div>
-@endsection
+        
+    @endsection
